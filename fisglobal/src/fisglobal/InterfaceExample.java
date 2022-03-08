f2032b5d66eab0286dc769bdf2387b6af2fa2791
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2022-03-08 10:36:53 +0530

    Add files via upload

--- fisglobal/src/fisglobal/InterfaceExample.java
+++ fisglobal/src/fisglobal/InterfaceExample.java
@@ -4,7 +4,16 @@ interface Shop{
 	int x=10;  // constants it is a final variable
 	void selectGoods();
 	void payment();
+	public default void generateOtp() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter OTP for completing the trasnactions..");
+		
+	}
 	public abstract void shipment(); 
+	
+		public static void registration() {
+			System.out.println("Shop must be registered first ...");
+		}
 }
 
 class ManualShops implements Shop{
@@ -29,6 +38,8 @@ class ManualShops implements Shop{
 		System.out.println("Goods should carry on your own");
 		
 	}
+
+	
 	
 }
 
@@ -54,6 +65,14 @@ class CreditDebitShops implements Shop{
 		System.out.println("It can be done through dtdc ");
 		
 	}
+	@Override
+	public void generateOtp() {
+		// TODO Auto-generated method stub
+		System.out.println("enter the grid values on your credit/debit card..");
+		
+	}
+
+	
 	
 }
 public class InterfaceExample {
@@ -61,15 +80,20 @@ public class InterfaceExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Shop.registration();
+		
 		CreditDebitShops shop1 = new CreditDebitShops();
+		
 		shop1.selectGoods();
 		shop1.payment();
+		shop1.generateOtp();
 		shop1.shipment();
 		
 		ManualShops shop2 = new ManualShops();
 		shop2.selectGoods();
 		shop2.payment();
 		shop2.shipment();
+		
 
 	}
 
