ccc19e80e647c7a6983c57855bd2723677785cd0
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2022-03-17 17:01:54 +0530

    Add files via upload

---
+++ logginexample/src/main/java/logginexample/TestLoggin.java
@@ -0,0 +1,21 @@
+package logginexample;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestLoggin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logger log = Logger.getLogger(TestLoggin.class.getName());
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		log.info("Object is going to initialize by IoC");
+		
+		Employee emp = (Employee)context.getBean("emp");
+		log.info("emp object is created....");
+		emp.display();
+		log.info("display method is called on emp object...");
+	}
+
+}
