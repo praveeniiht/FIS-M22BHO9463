122c2347a03e47bf8bff32d9e123ae7ddfa77cd1
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2022-03-24 14:48:08 +0530

    Add files via upload

---
+++ rest-uri-path-regex/src/main/java/com/logicbig/example/OrderService.java
@@ -0,0 +1,46 @@
+package com.logicbig.example;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+
+@Path("orders")
+public class OrderService {
+
+    @GET
+    @Path("{orderId:\\d{3,5}}")  // mmi 3 digit to max 5 digit
+    public String getOrders(@PathParam("orderId") String orderId) {
+        return "orderId: " + orderId;
+    }
+
+    @GET
+    @Path("{orderId:[a-z]\\d{2,3}}")
+    public String getOrders2(@PathParam("orderId") String orderId) {
+        return "orderId: " + orderId;
+    }
+    
+    @GET
+    @Path("/test")
+    public String testQueryParam(
+            @DefaultValue("praveen")
+            @QueryParam("name") String name, 
+            @DefaultValue("Hyderabad")
+            @QueryParam("city") String city
+    		) {
+        return "Name= " + name+" City  = "+city;
+    }
+    
+    
+    @GET
+    @Path("/test1")
+    public String testQueryParam1(
+       
+            @QueryParam("name") String name, 
+            
+            @QueryParam("city") String city
+    		) {
+        return "Name= " + name+" City  = "+city;
+    }
+}
